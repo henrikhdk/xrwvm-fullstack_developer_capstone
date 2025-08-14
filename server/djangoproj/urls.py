752eba5569bb37d2100cd782c9e8564d6739ee8f
Henrik D'oark <henrikdoark@gmail.com>
2025-08-14 12:57:49 -0400

    Add registration functionality with corresponding view and URL routing

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('logout/', TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('register/', TemplateView.as_view(template_name="index.html")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static('/', document_root=os.path.join(settings.BASE_DIR, 'frontend/build'))
